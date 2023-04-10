aafdb5ab449cc2be6324fe6e5bb5125f125f7444
Ximena Hidalgo <122484522+XimenaFHH@users.noreply.github.com>
2023-04-09 21:15:32 -0600

    Project
    Aquí se encuentran los Assets, Package y ProyectSettings de todo el proyecto

--- SistemadeMazmorras3D/Assets/Scripts/MAZMORRAS3D/RoomSpawner.cs
+++ SistemadeMazmorras3D/Assets/Scripts/MAZMORRAS3D/RoomSpawner.cs
@@ -3,75 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class RoomSpawner : MonoBehaviour
 {
-    public int openSide;
-    // 1 Need Bottom door.
-    // 2 Nedd Top door.
-    // 3 Need Left door.
-    // 4 Need Right door.
+    public int openSide; // 1 Need Bottom door, 2 Need Top door, 3 Need Left door, 4 Need Right door
 
-   
     private RoomTemplates templates;
     private int rand;
     private bool spawned = false;
 
-    void Start()
+    private void Start()
     {
         templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
-        Invoke("Spawn", 0.1f);
+        Invoke(nameof(Spawn), 0.1f);
     }
 
-    void Spawn()
+    private void Spawn()
     {
-        if (spawned == false)
+        if (spawned) return;
+
+        switch (openSide)
         {
-            if (openSide == 1)
-            {
-                //Need Bottom door.
+            case 1: // Need Bottom door
                 rand = Random.Range(0, templates.bottomRooms.Length);
                 Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
-            }
-            else if (openSide == 2)
-            {
-                //Need Top door.
+                break;
+
+            case 2: // Need Top door
                 rand = Random.Range(0, templates.topRooms.Length);
                 Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
-            }
-            else if (openSide == 3)
-            {
-                //Need Left door.
+                break;
+
+            case 3: // Need Left door
                 rand = Random.Range(0, templates.leftRooms.Length);
                 Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
-            }
-            else if (openSide == 4)
-            {
-                //Need Right door.
+                break;
+
+            case 4: // Need Right door
                 rand = Random.Range(0, templates.rightRooms.Length);
                 Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                break;
+        }
 
-            }
-            templates.IncrementRoomCount();
+        templates.IncrementRoomCount();
+        spawned = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("SpawnPoint")) return;
+
+        var otherSpawner = other.GetComponent<RoomSpawner>();
+        if (otherSpawner != null && !otherSpawner.spawned && !spawned)
+        {
+            Instantiate(templates.closedRoom, transform.position, Quaternion.identity); 
+            Destroy(gameObject); 
         }
 
         spawned = true;
-
-    
-
     }
-
-    public void OnTriggerEnter(Collider other)
-    {
-       if (other.CompareTag("SpawnPoint"))
-        {
-            if (other.GetComponent<RoomSpawner>().spawned==false && spawned==false)
-            {
-                Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
-                Destroy(gameObject);
-                
-            }
-            spawned = true;
-        }
-    }
-
 }
